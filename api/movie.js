8b96396bf4083f05bd1c339e1319f96f79e982da
Esther <bonjourjj3@gmail.com>
2024-09-25 13:20:54 +0900

    Add Serverless Functions

---
+++ api/movie.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const { OMDB_API_KEY } = process.env;
+
+export default async function handler(request, response) {
+  // request: server로 요청이 들어올 때 그 요청 정보가 들어있는 매개변수
+  // response: server에서 응답을 해줄 때 사용할 수 있는 여러가지 methods가 들어있는 매개변수
+  const { title, page, id } = JSON.parse(request.body);
+  const url = id
+    ? `https://omdbapi.com?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
+    : `https://omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}&page=${page}`;
+  const res = await fetch(url);
+  const json = await res.json();
+  response.status(200).json(json);
+}
--- src/store/movie.js
+++ src/store/movie.js
@@ -23,9 +23,13 @@ export const searchMovies = async (page) => {
   }
 
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=1505aff6&s=${store.state.searchText}&page=${page}`
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page,
+      }),
+    });
     const { Search, totalResults, Response, Error } = await res.json();
 
     if (Response === "True") {
@@ -44,9 +48,12 @@ export const searchMovies = async (page) => {
 
 export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=1505aff6&i=${id}&plot=full`
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        id,
+      }),
+    });
     store.state.movie = await res.json();
   } catch (error) {
     console.log("getMovieDetails error: ", error);
