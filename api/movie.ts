9c1f737c4a04ff424eaba9325ca492d1df0fc18f
Esther <bonjourjj3@gmail.com>
2024-09-30 16:05:19 +0900

    OpenAI 챗봇 기능 추가

--- api/movie.ts
+++ api/movie.ts
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const { OMDB_API_KEY } = process.env;
+const { OMDB_APIKEY } = process.env;
 
 export default async function handler(
   request: VercelRequest,
@@ -11,8 +11,8 @@ export default async function handler(
   // response: server에서 응답을 해줄 때 사용할 수 있는 여러가지 methods가 들어있는 매개변수
   const { title, page, id } = JSON.parse(request.body);
   const url = id
-    ? `https://omdbapi.com?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
-    : `https://omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}&page=${page}`;
+    ? `https://omdbapi.com?apikey=${OMDB_APIKEY}&i=${id}&plot=full`
+    : `https://omdbapi.com?apikey=${OMDB_APIKEY}&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
   response.status(200).json(json);
